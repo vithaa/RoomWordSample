7a9cce51f4a1d53030487bf647458c62f4a6f2b4
Katonjr <Ciplukml80@gmail.com>
2023-10-16 00:50:05 +0700

    praktek 10

---
+++ app/src/main/java/com/vitha/roomwordsample/WordViewModel.kt
@@ -0,0 +1,22 @@
+package com.vitha.roomwordsample
+import WordRepository
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+class WordViewModel(private val repository: WordRepository) : ViewModel() {
+    val allWords: LiveData<List<Word>> = repository.allWords.asLiveData()
+    fun insert(word: Word) = viewModelScope.launch {
+        repository.insert(word)
+    }
+}
+class WordViewModelFactory(private val repository: WordRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(WordViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return WordViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
